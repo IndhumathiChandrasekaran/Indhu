50f6b770125d746696368d8058b5a586a324dfb0
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-18 22:01:39 +0530

    Add files via upload

---
+++ primeNumBet2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class primeNumBet2 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Start :");
+		int start = s.nextInt();
+		System.out.println("End :");
+		int end = s.nextInt();
+		
+		
+		for (int i = start; i <= end; i++) {
+			int j;
+			for (j = 2; j < i; j++) {
+				if (i % j == 0) {
+					break;
+				}
+			}
+			if (i == j) {
+				System.out.println(i + " prime");
+			}
+
+		}
+
+	}
+}
