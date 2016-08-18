ddaaf69650d2792bc1714e5ee2321100135c9934
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-18 21:17:58 +0530

    Add files via upload

---
+++ EvenNum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class EvenNum {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Start :");
+	int start=s.nextInt();
+	System.out.println("End :");
+	int end=s.nextInt();
+	for(int i=start+1;i<end;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.println(i);
+		}
+	}
+	
+}
+}
