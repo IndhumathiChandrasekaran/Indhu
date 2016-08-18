5d3c862ecc8d9afedd8e0705f34f5990636f4984
IndhumathiChandrasekaran <indhumathi.ccse@ksrce.ac.in>
2016-08-18 22:00:11 +0530

    Add files via upload

--- EvenNum.java
+++ EvenNum.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
-
 public class EvenNum {
-public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
-	System.out.println("Start :");
-	int start=s.nextInt();
-	System.out.println("End :");
-	int end=s.nextInt();
-	for(int i=start+1;i<end;i++)
-	{
-		if(i%2==0)
-		{
-			System.out.println(i);
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Start :");
+		int start = s.nextInt();
+		System.out.println("End :");
+		int end = s.nextInt();
+		
+		
+		for (int i = start; i <= end; i++) {
+			int j;
+			for (j = 2; j < i; j++) {
+				if (i % j == 0) {
+					break;
+				}
+			}
+			if (i == j) {
+				System.out.println(i + " prime");
+			}
+
 		}
+
 	}
-	
-}
 }
